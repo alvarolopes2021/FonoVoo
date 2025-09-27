4d983b2aa8cfd8ea2f7543a77e841f92f42a02d8
alvaro lopes <alvarolopes2020@protonmail.com>
2025-09-27 19:12:29 -0300

    fix: change app name in multitab apps screen

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -29,7 +29,7 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Fono Voo',
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           backgroundColor: Color.fromRGBO(60, 120, 172, 1),
